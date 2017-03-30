28a10e3c840868db847a6873e496cac45c002822
vit_vel <lgp500optimusone>
2017-03-30 12:28:54 +0300

    init

---
+++ PCMRtree.h
@@ -0,0 +1,7 @@
+#ifndef PCM_R_TREE_PCMRTREE_H
+#define PCM_R_TREE_PCMRTREE_H
+
+struct PCMRtree
+{};
+
+#endif //PCM_R_TREE_PCMRTREE_H
---
+++ Rtree.h
@@ -0,0 +1,11 @@
+#ifndef PCM_R_TREE_RTREE_H
+#define PCM_R_TREE_RTREE_H
+
+#include <vector>
+
+constexpr double myexpr = 10e5 / 10e2 * 23 / 11;
+
+struct Rtree
+{};
+
+#endif //PCM_R_TREE_RTREE_H
